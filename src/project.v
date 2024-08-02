2df0943e1b1f50f387e8ed6d51393f0e0aaa887d
williamli0707 <williamhli0707@gmail.com>
2024-08-02 07:53:56 -0700

    project files

--- src/project.v
+++ src/project.v
@@ -6,9 +6,9 @@
 `default_nettype none
 
 module tt_um_neural_navigators (
-    input  wire [7:0] ui_in,    // Dedicated inputs
+    input  wire [7:0] ui_in,    // Dedicated inputs - pixels / biases
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
+    input  wire [7:0] uio_in,   // IOs: Input path - weights
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
